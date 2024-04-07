75693ac6ddaa41894bc6c625f945a1c9dd7abb47
Dams <rifflartdamien68@gmail.com>
2024-04-07 12:43:01 +0200

    v0.3
    Can now get how many times a student have each subjects for the next 99 days

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 const {Skolengo} = require('scolengo-api')
-const { writeFileSync } = require('node:fs')
+const { writeFileSync, writeFile } = require('node:fs')
 const config = require('./config');
-
-// index: 8= next monday | 9= next tuesday | 10= next wednesday ... 14 = next sunday
-// if the nextDay is the current day -> it outputs today
-function nextDay(index) {
-  var today = new Date();
-  var weekday = today.getDay();
-  var daysBefore = index - weekday;
-  if (daysBefore === 7) {
-    return today;
-  } else {
-    var nextDay = new Date(today.getTime() + daysBefore * 24 * 60 * 60 * 1000);
-    return nextDay;
-  }
-}
 
 subjects = [
   "ENS. MORAL & CIVIQUE",
@@ -31,6 +17,11 @@ subjects = [
   "SNT",
 ]
 
+function count(subject, text) {
+  var matches = text.match(new RegExp(`DESCRIPTION:${subject}`, 'g'));
+  return matches ? matches.length : 0;
+}
+
 Skolengo.fromConfigObject(config).then(async user => {
   const startDate = new Date(new Date()).toISOString().split('T')[0];
   const endDate = new Date(new Date(startDate).setDate(new Date(startDate).getDate() + 99)).toISOString().split('T')[0];
@@ -39,6 +30,9 @@ Skolengo.fromConfigObject(config).then(async user => {
   const agenda = await user.getAgenda(infoUser.id, startDate, endDate, 100)
   const agendaText = agenda.toICalendar()
 
-  var count = ((agendaText.match(/MATHEMATIQUES/g) || []).length)/2;
-  console.log(count)
+  writeFileSync('export.ics', agenda.toICalendar())
+
+  subjects.forEach((subjectName) => {
+    console.log(subjectName + ": " + count(subjectName, agendaText));
+  });
 })
