4a734026aefde05e9667c4d46d858a5d5106951c
Dams <rifflartdamien68@gmail.com>
2024-04-22 09:59:29 +0200

    v1.1
    - Loading screen

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -6,6 +6,7 @@ function App(): JSX.Element {
   // useState: état, permet de pas reset une variable à chaque fois que qq chose change | recalcule le component en fonction
   // de ce qui a changé. ex: bouton qui incrémente à chaque fois
   // primitive: <Subject[]> utilise une interface pour dire ce que c'est avant
+  const [loading, setLoading] = useState(true)
   const [data, setData] = useState<Data[]>([]);
   const [totalHours, setTotalHours] = useState<string>();
   const [totalDays, setTotalDays] = useState<number>();
@@ -14,6 +15,7 @@ function App(): JSX.Element {
   useEffect(() => {
     window.electron.ipcRenderer.invoke('dataRequest')
       .then((response) => {
+        setLoading(false);
         setData(response.data);
         setTotalHours(response.totalHours);
         setTotalDays(response.totalDays)
@@ -22,12 +24,20 @@ function App(): JSX.Element {
 
   return (
     // fragments: grouper les élements entre eux sans faire une autre div
-    <>
-      {data.map((subject) => 
-        <SubjectElement subject={subject}/>,
+    <div>
+      {loading ? (
+        <p className='text-3xl flex justify-center items-center h-screen w-screen'>
+          <span className="text-animation">⏳ Chargement ...</span>
+        </p>
+      ) : (
+        <>
+          {data.map((subject) => 
+            <SubjectElement subject={subject}/>,
+          )}
+          <p className='text-lg ml-3 mt-3'> <strong>TEMPS TOTAL</strong>: <span className='text-indigo-400'>{totalHours}</span> donc <span className='text-indigo-400'>{totalDays}</span> jours!</p>
+        </>
       )}
-      <p className='text-lg ml-3 mt-3'> <strong>TEMPS TOTAL</strong>: <span className='text-indigo-400'>{totalHours}</span> donc <span className='text-indigo-400'>{totalDays}</span> jours!</p>
-    </>
+    </div>
   )
 }
 
